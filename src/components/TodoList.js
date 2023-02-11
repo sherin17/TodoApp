3e01bd0f973ff6fdfe017be2bcac402d1c9740c7
sherin17 <112325140+sherin17@users.noreply.github.com>
2023-02-11 13:57:50 +0530

    first commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,51 +1,25 @@
-import { Button, Container, TextField } from '@mui/material'
-import React, { useState } from 'react'
+import { Button, Container, TextField, Typography } from '@mui/material'
+import useForm from './todo'
 import './TodoList.css'
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-
 const TodoList = () => {
 
     //useState
-        var [todo, settodo] = useState('')
-        var [todos, settodos] = useState([])
-       
-    
-    //ADD
-        const addtodo = () => {
-            if(todo !==''){
-                settodos([...todos,  {list : todo , id : Date.now()}])
-                settodo('')
-            }
-            
-        }
-    
-//DELETE
-    const onDelete = (id) => {
-    settodos(todos.filter((to) => to.id !== id))
-  }
-    
+     
+    var [todo,settodos] = useForm({name:"",quantity:"",description:""})
+
     return (
         <div className='container'>
             <Container maxWidth="lg" align='center'>
                 <h1>TODO LIST</h1>
                 <form className='form-group'>
-                    <TextField type="text" value={todo} label="Enter Your TODO" variant="outlined" fullWidth onChange={(event) => settodo(event.target.value)}></TextField>
-                    <Button variant="contained" color='error' onClick={addtodo}>ADD</Button>
+                    <TextField name='name' value={todo.name} label="TODO Name" variant="outlined" fullWidth onChange={settodos}></TextField><br></br>
+                    <TextField name='quantity' value={todo.quantity} label="Quantity" variant="outlined" fullWidth onChange={ settodos}></TextField><br></br>
+                    <TextField name='description' value={todo.description} label="Description" variant="outlined" fullWidth onChange={settodos}></TextField><br></br>
+                    <Button variant="contained" color='error' >ADD</Button>
                 </form>
-                <div className='list'>
-                    <ol>
-                        {
-                            todos.map((to) => (
-                                <li className='listitems'>
-                                <div className='list-item-list'>{to.list}</div>
-                                <span>
-                                    <DeleteOutlineIcon className='icons' id='delete' title="Delete" onClick={()=>onDelete(to.id)}/>
-                                </span>
-                            </li>
-                            ))
-                        }
-                    </ol>
-                </div>
+                    <Typography variant='h6'>{todo.name}</Typography>
+                    <Typography variant='h6'>{todo.quantity}</Typography>
+                    <Typography variant='h6'>{todo.description}</Typography>
             </Container>
         </div>
     )
